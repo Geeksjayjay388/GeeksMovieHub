a87d0e77fc5d86dd9512b30d5989ea0cb35d8e08
jacob <jacobsihul911@gmail.com>
2025-07-21 02:42:02 +0300

    Deployment Fixed

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/GeeksMovieHub/', // 👈 Use your repo name here if deploying to GitHub Pages
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html', // 👈 This helps Vite know where to fallback
-      },
-    },
-  },
-  server: {
-    open: true,
-  },
-});
+})
